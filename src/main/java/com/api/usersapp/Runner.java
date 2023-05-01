eb0722c0613e412ba34c8cbef60445f8fa2286db
Marc0Franc0 <marcofranco340@gmail.com>
2023-04-30 23:25:46 -0300

    Cambios en la codificación de las contraseñas

--- src/main/java/com/api/usersapp/Runner.java
+++ src/main/java/com/api/usersapp/Runner.java
@@ -2,8 +2,8 @@ package com.api.usersapp;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.stereotype.Component;
-
 import com.api.usersapp.model.Authority;
 import com.api.usersapp.model.AuthorityName;
 import com.api.usersapp.model.User;
@@ -30,9 +30,14 @@ public class Runner implements CommandLineRunner {
         }
 
         if (this.userRepository.count() == 0) {
+
+            var encoders = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+          
             this.userRepository
             .save(
-            new User("marcoof","password", List.of(this.authorityRepository.findByName(AuthorityName.ADMIN).get())));
+            new User("marcoof",encoders.encode("password"),
+             List.of(this.authorityRepository.findByName(AuthorityName.ADMIN).get()))
+             );
         }
 
 
--- src/main/java/com/api/usersapp/config/WebSecurityConfig.java
+++ src/main/java/com/api/usersapp/config/WebSecurityConfig.java
@@ -2,12 +2,8 @@ package com.api.usersapp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.configurers.provisioning.InMemoryUserDetailsManagerConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class WebSecurityConfig {
@@ -24,7 +20,7 @@ var user = User
 
     @Bean
     public PasswordEncoder passwordEncoder(){
-    return NoOpPasswordEncoder.getInstance();
+    return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     } 
 
 }
